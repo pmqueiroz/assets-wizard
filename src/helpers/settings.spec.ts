5c416e3d6afcac322db12a6822048fa57f676fdc
pmqueiroz <pedroqueiroz593@gmail.com>
2023-10-19 14:05:26 -0300

    style: fix text code styles

--- src/helpers/settings.spec.ts
+++ src/helpers/settings.spec.ts
@@ -5,32 +5,54 @@ describe('getSettings', () => {
     it('should be a function', () => {
         expect(getSettings).toBeInstanceOf(Function)
     })
-    
+
     describe('all values setted with wrong repo pattern', () => {
         beforeAll(() => {
-            utils.__stubPluginData({ repo: 'worng-pattern-name', token: 'some token', eventType: 'event' })
+            utils.__stubPluginData({
+                repo: 'worng-pattern-name',
+                token: 'some token',
+                eventType: 'event'
+            })
         })
 
         it('should get all the settings data and return in a object', () => {
             const settings = getSettings()
-    
-            expect(settings).toStrictEqual(expect.objectContaining({ repo: 'worng-pattern-name', token: 'some token', eventType: 'event' }))
+
+            expect(settings).toStrictEqual(
+                expect.objectContaining({
+                    repo: 'worng-pattern-name',
+                    token: 'some token',
+                    eventType: 'event'
+                })
+            )
         })
 
         it('should validate if passed valid config as true', () => {
-            expect(() => getSettings({ validate: true })).toThrowError('The repo you passed does not match the pattern [username]/[repository]')
+            expect(() => getSettings({ validate: true })).toThrowError(
+                'The repo you passed does not match the pattern [username]/[repository]'
+            )
         })
     })
 
     describe('all values setted correctly', () => {
         beforeAll(() => {
-            utils.__stubPluginData({ repo: 'right/pattern', token: 'some token', eventType: 'event' })
+            utils.__stubPluginData({
+                repo: 'right/pattern',
+                token: 'some token',
+                eventType: 'event'
+            })
         })
 
         it('should get all the settings data and return in a object', () => {
             const settings = getSettings()
-    
-            expect(settings).toStrictEqual(expect.objectContaining({ repo: 'right/pattern', token: 'some token', eventType: 'event' }))
+
+            expect(settings).toStrictEqual(
+                expect.objectContaining({
+                    repo: 'right/pattern',
+                    token: 'some token',
+                    eventType: 'event'
+                })
+            )
         })
 
         it('should validate if passed valid config as true', () => {
@@ -45,12 +67,18 @@ describe('getSettings', () => {
 
         it('should get all the settings data and return in a object', () => {
             const settings = getSettings()
-    
-            expect(settings).toStrictEqual({ repo: undefined, token: undefined, eventType: undefined })
+
+            expect(settings).toStrictEqual({
+                repo: undefined,
+                token: undefined,
+                eventType: undefined
+            })
         })
 
         it('should validate if passed valid config as true', () => {
-            expect(() => getSettings({ validate: true })).toThrowError('Your need to config all the settings first')
+            expect(() => getSettings({ validate: true })).toThrowError(
+                'Your need to config all the settings first'
+            )
         })
     })
 })
