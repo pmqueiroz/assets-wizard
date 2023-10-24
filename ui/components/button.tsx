7ae19d6db0abc6fbc8430373d8fe2a600fae3527
Pedro Queiroz <pedroqueiroz593@gmail.com>
2023-10-24 18:14:35 -0300

    feat: add loading when exporting (#9)

--- ui/components/button.tsx
+++ ui/components/button.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant: 'primary' | 'neutral'
     expand?: boolean
+    loading?: boolean
 }
 
 const variantStyle: Record<ButtonProps['variant'], string> = {
@@ -10,11 +11,34 @@ const variantStyle: Record<ButtonProps['variant'], string> = {
     primary: 'bg-purple-500 text-white'
 }
 
+const Loading = () => (
+    <svg
+        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+    >
+        <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+        ></circle>
+        <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+    </svg>
+)
+
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ variant, expand, ...rest }, ref) => {
+    ({ variant, expand, loading, children, ...rest }, ref) => {
         return (
             <button
-                className={`h-12 px-4 py-3 gap-1 flex flex-col justify-center items-center text-base font-semibold rounded-lg ${
+                className={`h-12 px-4 py-3 gap-1 flex justify-center items-center text-base font-semibold rounded-lg ${
                     variantStyle[variant]
                 } ${expand ? 'w-full' : ''}
                 disabled:opacity-40
@@ -22,7 +46,10 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
                 style={{ minWidth: '48px' }}
                 {...rest}
                 ref={ref}
-            />
+            >
+                {loading && <Loading />}
+                {children}
+            </button>
         )
     }
 )
